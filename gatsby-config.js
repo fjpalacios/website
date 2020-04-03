8aef0a2d727f28e564df0ac616bfd83498590488
Javi Palacios <javi@fjp.es>
2020-04-04 00:32:52 +0200

    feat: add multilingual support
    Closes #1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: ["gatsby-plugin-typescript"],
+  plugins: [
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-intl',
+      options: {
+        path: `${__dirname}/locales`,
+        languages: ['en', 'es'],
+        defaultLanguage: 'en',
+        redirect: false,
+      },
+    },
+  ],
 }
