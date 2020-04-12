e5a2bfaad7cc9d5746b16c62c8d8624e33af2745
Javi Palacios <javi@fjp.es>
2020-04-12 18:50:47 +0200

    test(languageslist): fix tests' naming (#15)

--- src/components/__tests__/languages-list.spec.tsx
+++ src/components/__tests__/languages-list.spec.tsx
@@ -3,7 +3,7 @@ import createComponentWithIntl from '../../../__helpers__/i18n-component'
 import Faker from 'faker'
 import React from 'react'
 
-describe('TextArea', () => {
+describe('LanguagesList', () => {
   Faker.seed(123)
   const data: languagesList[] = [
     {
@@ -14,27 +14,29 @@ describe('TextArea', () => {
   ]
 
   it('should render properly', () => {
-    const textArea = createComponentWithIntl(<LanguagesList data={data} />)
-    expect(textArea).toMatchSnapshot()
+    const languagesList = createComponentWithIntl(<LanguagesList data={data} />)
+    expect(languagesList).toMatchSnapshot()
   })
 
   it('should show the name', () => {
-    const textArea = createComponentWithIntl(<LanguagesList data={data} />)
-    const name = textArea.find('.languages-list__name span').text()
+    const languagesList = createComponentWithIntl(<LanguagesList data={data} />)
+    const name = languagesList.find('.languages-list__name span').text()
     expect(name).toBe(data[0].name)
   })
 
   it('should show a url if it exists', () => {
     data[0].url = 'https://duckduckgo.com'
-    const textArea = createComponentWithIntl(<LanguagesList data={data} />)
-    const name = textArea.find('.languages-list__name a').text()
+    const languagesList = createComponentWithIntl(<LanguagesList data={data} />)
+    const name = languagesList.find('.languages-list__name a').text()
     expect(name).toBe(data[0].name)
     data[0].url = undefined
   })
 
   it('should show a list of languages', () => {
-    const textArea = createComponentWithIntl(<LanguagesList data={data} />)
-    const location = textArea.find('.languages-list__languages li a').text()
+    const languagesList = createComponentWithIntl(<LanguagesList data={data} />)
+    const location = languagesList
+      .find('.languages-list__languages li a')
+      .text()
     expect(location).toBe(`#${data[0].languages[0]}`)
   })
 
@@ -53,8 +55,8 @@ describe('TextArea', () => {
   })
 
   it('should show the description', () => {
-    const textArea = createComponentWithIntl(<LanguagesList data={data} />)
-    const desc = textArea.find('.languages-list__desc').text()
+    const languagesList = createComponentWithIntl(<LanguagesList data={data} />)
+    const desc = languagesList.find('.languages-list__desc').text()
     expect(desc).toBe(data[0].desc)
   })
 })
