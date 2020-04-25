4e833f89b0d059755ff491959b3cd1dc9407249d
Javi Palacios <javi@fjp.es>
2020-04-25 15:16:33 +0200

    feat(seo): add SEO stuff
    Closes #38

--- cypress/e2e/home.spec.ts
+++ cypress/e2e/home.spec.ts
@@ -5,6 +5,58 @@ describe('Home', () => {
     cy.visit('/').get('main')
   })
 
+  it('should have valid English SEO properties', () => {
+    cy.get('html').should('have.attr', 'lang', 'en')
+    cy.get('head title').should('have.text', 'Francisco Javier Palacios Pérez')
+    cy.get('head meta[property="og:url"]').should(
+      'have.attr',
+      'content',
+      'https://fjp.es/'
+    )
+    cy.get('head meta[property="og:type"]').should(
+      'have.attr',
+      'content',
+      'website'
+    )
+    cy.get('head meta[name="description"]').should(
+      'have.attr',
+      'content',
+      "Francisco Javier Palacios Pérez's personal website, Software Developer based in Valencia (Spain)"
+    )
+    cy.get('head meta[name="image"]').should(
+      'have.attr',
+      'content',
+      'https://fjp.es/logo.jpg'
+    )
+  })
+
+  it('should have valid Spanish SEO properties', () => {
+    cy.get('.header__subtitle').should('have.text', 'Software Developer')
+    cy.get('.menu .menu__left .language-switcher img').click()
+    cy.get('html').should('have.attr', 'lang', 'es')
+    cy.get('head title').should('have.text', 'Francisco Javier Palacios Pérez')
+    cy.get('head meta[property="og:url"]').should(
+      'have.attr',
+      'content',
+      'https://fjp.es/es/'
+    )
+    cy.get('head meta[property="og:type"]').should(
+      'have.attr',
+      'content',
+      'website'
+    )
+    cy.get('head meta[name="description"]').should(
+      'have.attr',
+      'content',
+      'Página web personal de Francisco Javier Palacios Pérez, desarrollador de software de Valencia (España)'
+    )
+    cy.get('head meta[name="image"]').should(
+      'have.attr',
+      'content',
+      'https://fjp.es/logo.jpg'
+    )
+  })
+
   describe('Menu bar', () => {
     it('should have a functional language switcher', () => {
       cy.get('.header__subtitle').should('have.text', 'Software Developer')
