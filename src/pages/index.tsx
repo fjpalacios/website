1a58c972c816317406aa8c42f1b828b0ba50075c
Javi Palacios <javi@fjp.es>
2020-04-25 15:26:52 +0200

    feat(seo): add SEO stuff (#78)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from 'react'
 import { About } from '../components/about'
 import { Layout } from '../components/layout'
+import { Seo } from '../components/seo'
 
 export default (): ReactElement => (
   <Layout>
+    <Seo />
     <About />
   </Layout>
 )
