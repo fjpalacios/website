e7ee1da7f0ed2ba1ed1359d9aebe6d65e4ac67cd
Javi Palacios <javi@fjp.es>
2020-04-25 22:28:44 +0200

    feat: add about page
    Closes #37

---
+++ src/components/about.tsx
@@ -0,0 +1,37 @@
+import './about.scss'
+import React, { FunctionComponent, ReactElement } from 'react'
+import aboutEn from '../../content/en/about'
+import aboutEs from '../../content/es/about'
+import { TextArea } from './text-area'
+import { Title } from './title'
+import { useIntl } from 'gatsby-plugin-intl'
+
+export const About: FunctionComponent = (): ReactElement => {
+  const intl = useIntl()
+  const locale = intl.locale
+  const data: { [key: string]: { [key: string]: any } } = {
+    me: {
+      en: aboutEn.me,
+      es: aboutEs.me,
+    },
+    internet: {
+      en: aboutEn.internet,
+      es: aboutEs.internet,
+    },
+  }
+
+  return (
+    <main className="about">
+      <section className="about__grid">
+        <section className="about__grid__me">
+          <Title title={intl.formatMessage({ id: 'about.me' })} />
+          <TextArea text={data.me[locale]} />
+        </section>
+        <section className="about__grid__internet">
+          <Title title={intl.formatMessage({ id: 'about.internet' })} />
+          <TextArea text={data.internet[locale]} />
+        </section>
+      </section>
+    </main>
+  )
+}
