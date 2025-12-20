3d5f454c62456a038b9177db3c7d7f800a6a3d8d
Javi Palacios <javi@fjp.es>
2025-12-20 10:55:12 +0100

    feat(share): add Share component to all post detail pages

---
+++ src/content/static/share/en.js
@@ -0,0 +1,11 @@
+export default `
+  <p>
+    If you have come this far and have something to say you can contact me via
+    <a href="https://twitter.com/fjpalacios">Twitter</a> or send me an
+    <a href="mailto:javi@fjp.es">email</a>. You can also share this page with your friends via
+    <a href="https://twitter.com/intent/tweet?url=%URL%&text=%TITLE%&via=%TWITTER%" target='_blank'>Twitter</a>,
+    <a href="https://www.facebook.com/sharer/sharer.php?u=%URL%" target="_blank">Facebook</a>,
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url=%URL%" target="_blank">LinkedIn</a>
+    or <a href="mailto:?subject=%TITLE%&body=%TITLE%: %URL%">email</a>.
+  </p>
+  `;
---
+++ src/content/static/share/es.js
@@ -0,0 +1,11 @@
+export default `
+  <p>
+    Si has llegado hasta aquí y tienes algo que decir puedes escribirme por
+    <a href="https://twitter.com/fjpalacios">Twitter</a> o enviarme un
+    <a href="mailto:javi@fjp.es">correo electrónico</a>. También puedes compartir esta página
+    con tus amigos a través de <a href="https://twitter.com/intent/tweet?url=%URL%&text=%TITLE%&via=%TWITTER%" target='_blank'>Twitter</a>,
+    <a href="https://www.facebook.com/sharer/sharer.php?u=%URL%" target="_blank">Facebook</a>,
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url=%URL%" target="_blank">LinkedIn</a>
+    o <a href="mailto:?subject=%TITLE%&body=%TITLE%: %URL%">correo electrónico</a>.
+  </p>
+  `;
