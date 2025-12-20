6b6f442306886ac78ad34a5a2bd35335020f0580
Javi Palacios <javi@fjp.es>
2025-12-20 10:56:11 +0100

    feat(share): update social sharing links and add WhatsApp support
    - Update LinkedIn share URL to modern API endpoint
    - Add WhatsApp sharing option for both Spanish and English
    - Add rel='noopener noreferrer' to all external share links for security
    - Instagram sharing not added (not supported for web URLs)

--- src/content/static/share/en.js
+++ src/content/static/share/en.js
@@ -3,9 +3,10 @@ export default `
     If you have come this far and have something to say you can contact me via
     <a href="https://twitter.com/fjpalacios">Twitter</a> or send me an
     <a href="mailto:javi@fjp.es">email</a>. You can also share this page with your friends via
-    <a href="https://twitter.com/intent/tweet?url=%URL%&text=%TITLE%&via=%TWITTER%" target='_blank'>Twitter</a>,
-    <a href="https://www.facebook.com/sharer/sharer.php?u=%URL%" target="_blank">Facebook</a>,
-    <a href="https://www.linkedin.com/shareArticle?mini=true&url=%URL%" target="_blank">LinkedIn</a>
+    <a href="https://twitter.com/intent/tweet?url=%URL%&text=%TITLE%&via=%TWITTER%" target='_blank' rel='noopener noreferrer'>Twitter</a>,
+    <a href="https://www.facebook.com/sharer/sharer.php?u=%URL%" target="_blank" rel='noopener noreferrer'>Facebook</a>,
+    <a href="https://www.linkedin.com/sharing/share-offsite/?url=%URL%" target="_blank" rel='noopener noreferrer'>LinkedIn</a>,
+    <a href="https://api.whatsapp.com/send?text=%TITLE%%20%URL%" target="_blank" rel='noopener noreferrer'>WhatsApp</a>
     or <a href="mailto:?subject=%TITLE%&body=%TITLE%: %URL%">email</a>.
   </p>
   `;
--- src/content/static/share/es.js
+++ src/content/static/share/es.js
@@ -3,9 +3,10 @@ export default `
     Si has llegado hasta aquí y tienes algo que decir puedes escribirme por
     <a href="https://twitter.com/fjpalacios">Twitter</a> o enviarme un
     <a href="mailto:javi@fjp.es">correo electrónico</a>. También puedes compartir esta página
-    con tus amigos a través de <a href="https://twitter.com/intent/tweet?url=%URL%&text=%TITLE%&via=%TWITTER%" target='_blank'>Twitter</a>,
-    <a href="https://www.facebook.com/sharer/sharer.php?u=%URL%" target="_blank">Facebook</a>,
-    <a href="https://www.linkedin.com/shareArticle?mini=true&url=%URL%" target="_blank">LinkedIn</a>
+    con tus amigos a través de <a href="https://twitter.com/intent/tweet?url=%URL%&text=%TITLE%&via=%TWITTER%" target='_blank' rel='noopener noreferrer'>Twitter</a>,
+    <a href="https://www.facebook.com/sharer/sharer.php?u=%URL%" target="_blank" rel='noopener noreferrer'>Facebook</a>,
+    <a href="https://www.linkedin.com/sharing/share-offsite/?url=%URL%" target="_blank" rel='noopener noreferrer'>LinkedIn</a>,
+    <a href="https://api.whatsapp.com/send?text=%TITLE%%20%URL%" target="_blank" rel='noopener noreferrer'>WhatsApp</a>
     o <a href="mailto:?subject=%TITLE%&body=%TITLE%: %URL%">correo electrónico</a>.
   </p>
   `;
